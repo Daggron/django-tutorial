4ce5c7e620993e37947dce990d656cc079792fed
Daggron <returnofking04@gmail.com>
2018-09-05 23:32:08 +0530

    Basic Template for djago based music app

---
+++ DJANGO Tutoriasl/website/music/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.http import Http404
+from .models import Album
+from django.shortcuts import render
+def index(request):
+    all_album = Album.objects.all()
+    context={'all_album':all_album}
+    return  render(request,'music/index.html',context)
+def details(request,album_id):
+    try:
+        album=Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album Does not exist")
+    return render(request,'music/detail.html',{'album':album})
